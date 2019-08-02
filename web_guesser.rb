a5682324a5257947b3a0050f95e2774bb798b80a
Eric Stephenson <ericb.stephenson@gmail.com>
2019-08-02 06:47:36 -0700

    Number guessing game working

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,7 +2,21 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  number = rand(1..20)
+  number = rand(1..10)
 
-  erb :index, locals: { number: number }
+  if params['guess']
+    guess = params['guess'].to_i
+
+    if guess > number
+      erb :index, locals: { number: number, message: 'Too high!' }
+
+    elsif guess < number
+      erb :index, locals: { number: number, message: 'Too low!' }
+
+    elsif guess == number
+      erb :index, locals: { number: number, message: 'You got it!' }
+    end
+  else
+    erb :index, locals: { number: false, message: false }
+  end
 end
